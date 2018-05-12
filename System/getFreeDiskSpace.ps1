3746a5749854cdedf6d125cc7e0d0f95076c9122
t00148178 <tcc1011pm@yahoo.com>
2018-05-12 17:46:58 +0100

    add getFreeDiskSpace.ps1

---
+++ System/getFreeDiskSpace.ps1
@@ -0,0 +1,4 @@
+﻿Get-WMIObject Win32_Logicaldisk -filter "deviceid='C:'" -ComputerName localhost |
+Select PSComputername,DeviceID,
+@{Name="SizeGB";Expression={$_.Size/1GB -as [int]}},
+@{Name="FreeGB";Expression={[math]::Round($_.Freespace/1GB,2)}}
